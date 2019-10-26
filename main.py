2fac09afec4c0a45d1b4eaff6d65480bbcf8d531
Andreas Schmidt <schmidt@nt.uni-saarland.de>
2019-10-26 08:59:02 +0200

    += get local IP and subscribe for it

--- main.py
+++ main.py
@@ -4,19 +4,29 @@ from threading import Thread
 import requests
 from datetime import datetime
 import sys
+import socket
 from spannungsteiler.util import broker_util
 from spannungsteiler.app import SpannungsteilerApp
 from spannungsteiler.user import User, USERS
 
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("9.9.9.9", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
 def subscribe_to_topics():
     topics = [ "spannungsteiler_demand_publish",
                "spannungsteiler_fill_level_publish",
                "spannungsteiler_offer_publish" ]
+
+
     for topic in topics:
         broker_util.send("subscribe", {
             "sender": "spannungsteiler",
-            "address": "http://194.94.239.78:5000/spannungsteiler",
+            "address": "http://{}:5000/spannungsteiler".format(get_local_ip()),
             "interestedIn": topic
         })
 
