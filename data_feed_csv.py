39828fb46727abffae2c0783ee975f2778aa3fdc
Andreas Schmidt <schmidt@nt.uni-saarland.de>
2019-10-26 08:58:26 +0200

    ~= fix broken broker import in data_feed_csv

--- data_feed_csv.py
+++ data_feed_csv.py
@@ -1,4 +1,4 @@
-from broker_util import send_demand
+from spannungsteiler.util.broker_util import send_demand
 import pandas as pd
 from datetime import time
 from time import sleep
@@ -26,6 +26,6 @@ for index, row in dataframe.iterrows():
     time_list.append(row[1])
 
     time_stamp = time(time_list[0], time_list[1])
-    
+
     send_demand(USERID, timestamp, row[2])
     sleep(0.3)
